e1c0baeb1f365eba9052eeea9f516b204b5c9acd
jdw09 <razion0304@naver.com>
2025-04-12 19:08:22 +0900

    [Silver II] Title: 에디터, Time: 200 ms, Memory: 126016 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -4,18 +4,18 @@ arrL = deque(stdin.readline().rstrip())
 arrR = deque()
 
 for _ in range(int(stdin.readline().rstrip())):
-    cmd = stdin.readline().strip()
-    if cmd[0] == "P":
-        cmd, n = cmd.split()
-        arrL.append(n)
-    elif cmd == "L":
+    cmd = stdin.readline().strip() #명령어 입력
+    if cmd[0] == "P": #만약 P라면
+        cmd, n = cmd.split() #숫자 분리
+        arrL.append(n) #추가
+    elif cmd == "L": #L이면
+        if arrL: #arrL리스트가 공백이 아니라면
+            arrR.append(arrL.pop()) #arrL의 맨 뒷 인덱스를 arrR에 추가
+            arrR.rotate(1) #맨 뒷 인덱스를 맨 앞으로 추가
+    elif cmd == "D": #만약 D라면
+        if arrR: #arrR이 공백이 아니라면
+            arrL.append(arrR.popleft()) #arrL에 arrR의 첫번째 인덱스를 추출해서 추가
+    elif cmd == "B": #B면
         if arrL:
-            arrR.append(arrL.pop())
-            arrR.rotate(1)
-    elif cmd == "D":
-        if arrR:
-            arrL.append(arrR.popleft())
-    elif cmd == "B":
-        if arrL:
-            arrL.pop()
+            arrL.pop() #arrL의 가장 오른쪽 인덱스 삭제
 print("".join(arrL)+"".join(arrR))
